0b153efc2527f3f22367f6a26bdccf559b6d0471
ngumb <113451441+ngumb@users.noreply.github.com>
2023-09-18 14:15:17 +0200

    GUI Updated

--- SegmentationFunctions/ParticleSeparationWatershed.m
+++ SegmentationFunctions/ParticleSeparationWatershed.m
@@ -1,4 +1,4 @@
-function SepParticles = ParticleSeparationWatershed(MnonConv,Convexthresh,AspCutoff)
+function SepParticles = ParticleSeparationWatershed(MnonConv,Convexthresh)
 % Ultimate Erosion of Convex Shapes (UECS) based particle separation of non convex particles
 %   based on the discribtion of 
 %   Park, C. and Ding, Y. (2021) Data Science for Nano Image Analysis, Springer Nature, Cham, Switzerland
@@ -21,14 +21,14 @@ bw3(Ld2 == 0) = 0;
 
 BB=regionprops(bw3,'BoundingBox');
 BB = struct2cell(BB);
-stats = regionprops(bw3,'Perimeter','ConvexImage','MaxFeretProperties','MinFeretProperties');
+stats = regionprops(bw3,'Perimeter','ConvexImage');
 
 for ii=1:length(stats)
    cp = regionprops(+stats(ii).ConvexImage,'Perimeter');
    stats(ii).Convexity = cp(1).Perimeter / stats(ii).Perimeter;
    
 end
-AspR = [stats.('MaxFeretDiameter')]./[stats.('MinFeretDiameter')];
+
 Convex = [stats.Convexity]';
 clear stats
 Mstack = regionprops('table', bw3, 'Image');
@@ -43,33 +43,12 @@ for ii = 1:length(Mstack)
         Msplit = bw3(BBcurr(2):BBcurr(2)+BBcurr(4)-1,BBcurr(1):BBcurr(1)+BBcurr(3)-1,1);
         bw3(BBcurr(2):BBcurr(2)+BBcurr(4)-1,BBcurr(1):BBcurr(1)+BBcurr(3)-1,1) =...
             ~(Msplit==MnonConv);
-    elseif AspR(ii) < AspCutoff
-        MnonConv = Mstack{ii};
-        Msplit = bw3(BBcurr(2):BBcurr(2)+BBcurr(4)-1,BBcurr(1):BBcurr(1)+BBcurr(3)-1,1);
-        bw3(BBcurr(2):BBcurr(2)+BBcurr(4)-1,BBcurr(1):BBcurr(1)+BBcurr(3)-1,1) =...
-            ~(Msplit==MnonConv);
+    
     end
 end
 
 SepParticles = bw3;
-%% TEST CONVEXITY AND REMOVE NONCONVEX FROM IMAGE
-% SepParticles= {};
-% [B, L] = bwboundaries(bw3,8,'noholes');
-% num_markers = size(B,1);
-% 
-% for j = 1:num_markers
-%     pls = logical(L==j);
-% 
-%     s  = regionprops(pls,'ConvexImage','Perimeter','Area','ConvexArea');
-%     cp = regionprops(+s(1).ConvexImage,'Perimeter');
-%     Conv = cp(1).Perimeter / s(1).Perimeter;
-% 
-%     if Conv <= Convthresh 
-%         continue
-%     end
-% 
-%     SepParticles{j} = pls;
-% end
+
 
 % 
 end
